7c159a61f464264954568650527280dafe921742
julio958214 <julio958214@gmail.com>
2021-11-05 20:54:31 -0300

    Uso do find

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -16,4 +16,22 @@ describe('Esperas...', () =>{
         cy.get('#novoCampo').should('exist')
         cy.get('#novoCampo').type('funciona')
     })
+
+    it('Deve fazer retrys', ()=> {
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo')
+            // .should('not.exist')
+            .should('exist')
+            .type('funciona')
+    })
+
+    it.only('Uso do find', ()=> {
+        cy.get('#buttonList').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+
+            cy.get('#lista li span')
+            .should('contain', 'Item 2')
+    })
 })
